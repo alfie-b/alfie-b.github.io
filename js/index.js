e197f4751f5eed9042b719163361c99bf1b84c1e
alfie-b <alfie.balston@gmail.com>
2018-08-27 10:52:09 +0100

    fix map

--- js/index.js
+++ js/index.js
@@ -1,49 +1,60 @@
-
 // Softscroll
-$("#main-nav ul li a[href^='#']").on('click', function(e) {
-   e.preventDefault();
-   var hash = this.hash;
-   $('html, body').animate({
-       scrollTop: $(hash).offset().top
-     }, 300, function(){
-       window.location.hash = hash;
-     });
+$("#main-nav ul li a[href^='#']").on("click", function(e) {
+  e.preventDefault();
+  var hash = this.hash;
+  $("html, body").animate(
+    {
+      scrollTop: $(hash).offset().top
+    },
+    300,
+    function() {
+      window.location.hash = hash;
+    }
+  );
 });
 
 // Scrollspy settings
 var offset = 125;
-$('.navbar li a').click(function(event) {
-    event.preventDefault();
-    $($(this).attr('href'))[0].scrollIntoView();
-    scrollBy(0, -offset);
+$(".navbar li a").click(function(event) {
+  event.preventDefault();
+  $($(this).attr("href"))[0].scrollIntoView();
+  scrollBy(0, -offset);
 });
 
 // Map
-var penicuik = {lat: 55.830980, lng: -3.225040}
+var penicuik = { lat: 55.83098, lng: -3.22504 };
 var mapOptions = {
-	center: penicuik,
+  center: penicuik,
   zoom: 9,
-  disableDefaultUI: true,
-  };
+  disableDefaultUI: true
+};
 var map;
 function initMap() {
-	map = new google.maps.Map(document.getElementById('map'), mapOptions);
-  var marker = new google.maps.Marker({position: penicuik, map: map});
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  var marker = new google.maps.Marker({ position: penicuik, map: map });
 }
 
 // Form validation
 (function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    var forms = document.getElementsByClassName('needs-validation');
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
+  "use strict";
+  window.addEventListener(
+    "load",
+    function() {
+      var forms = document.getElementsByClassName("needs-validation");
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener(
+          "submit",
+          function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
 })();
